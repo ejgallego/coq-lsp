e71435b1de2d5b9906a91f7519df62689fd0a565
Vincent Laporte <Vincent.Laporte@fondation-inria.fr>
2019-02-13 12:16:20 +0000

    [default.nix] Build instructions

--- default.nix
+++ default.nix
@@ -8,8 +8,18 @@ let oc = ocaml-ng.ocamlPackages_4_07; in
 
 stdenv.mkDerivation {
   name = "coq-lsp-0";
+  src = builtins.filterSource (p: _: builtins.baseNameOf p != "_build") ./.;
   buildInputs = [
     dune oc.ocaml oc.findlib oc.num oc.yojson oc.cmdliner
     oc.lablgtk
   ];
+
+  preBuild = ''
+    for f in kernel dev/tools
+    do
+      patchShebangs coq/$f
+    done
+  '';
+
+  inherit (dune) installPhase;
 }
