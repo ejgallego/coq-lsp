f998846dad9b14d052afa35e5231eec4d45d8393
Emilio Jesus Gallego Arias <e+git@x80.org>
2019-02-13 18:34:42 +0100

    Add example.

---
+++ examples/ex0.v
@@ -0,0 +1,33 @@
+(* This example showcases some features of the early server *)
+Add Rec ML Path "../_build/install/default/lib/coq/plugins/".
+Add Rec LoadPath "../_build/install/default/lib/coq/theories/" as Coq.
+Add Rec LoadPath "../_build/install/default/lib/coq/plugins/" as Coq.
+
+From Coq Require Import Prelude.
+From Coq Require Import ssreflect ssrbool.
+From Coq Require Import Omega.
+
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+Lemma addn0 n : n + O = n.
+Proof. by elim: n. Qed.
+
+Lemma addnC n m : n + m = m + n.
+Proof. by elim n => //= ? ->. Qed.
+
+Lemma addnA n m l : n + m + l = m + (n + l).
+Proof. by elim n => //= ? ->. Qed.
+
+Lemma level1 : True.
+  Lemma level2 : True.
+  Proof. by []. Qed.
+exact: level2.
+Qed.
+
+Lemma broken : False.
+Proof. Qed.
+
+Lemma eq0 : 0 = 0.
+Proof. omega. Qed.
