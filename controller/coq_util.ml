504e5569dc65832e0b560e61dbe863c658f2398f
Emilio Jesus Gallego Arias <e+git@x80.org>
2022-07-01 12:53:32 +0200

    [diags] Undo error hack so error messages are good again.

--- controller/coq_util.ml
+++ controller/coq_util.ml
@@ -2,9 +2,8 @@ let coq_protect f x =
   try
     let res = f x in
     Ok res
-  with _exn ->
-    (* let e, info = Exninfo.capture exn in
-     * let loc = Loc.(get_loc info) in
-     * let msg = CErrors.iprint (e, info) in
-     * Error (loc, msg) *)
-    Error (None, Pp.mt ())
+  with exn ->
+    let e, info = Exninfo.capture exn in
+    let loc = Loc.(get_loc info) in
+    let msg = CErrors.iprint (e, info) in
+    Error (loc, msg)
