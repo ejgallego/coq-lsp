f1dca19770172df1d3232cb2e22e86695fea4bc2
Emilio Jesus Gallego Arias <e+git@x80.org>
2022-10-06 06:00:59 +0200

    [doc] Error recovery for failed Admitted .

--- fleche/doc.ml
+++ fleche/doc.ml
@@ -62,7 +62,8 @@ let parse_to_terminator : unit Pcoq.Entry.t =
   (* type 'a parser_fun = { parser_fun : te LStream.t -> 'a } *)
   let rec dot st =
     match Gramlib.LStream.next st with
-    | Tok.KEYWORD ("." | "..." | "Qed" | "Defined") | Tok.BULLET _ -> ()
+    | Tok.KEYWORD ("." | "..." | "Qed" | "Defined" | "Admitted") | Tok.BULLET _
+      -> ()
     | Tok.EOI -> ()
     | _ -> dot st
   in
