3b6595dafd5d8bdaac1e088538089f5da26e788d
Emilio Jesus Gallego Arias <e+git@x80.org>
2022-11-11 18:39:46 +0100

    [examples] Example showcasing Coq parsing state bug.
    c.f. https://github.com/coq/coq/issues/12575

---
+++ examples/pbroken.v
@@ -0,0 +1,11 @@
+(* See https://github.com/coq/coq/issues/12575 *)
+
+(* If you uncomment / comment this line, the whole setup becomes broken *)
+(* From Coq Require Import ssreflect. *)
+
+Ltac CaseEq name :=
+  generalize (refl_equal name); pattern name at -1 in |- *; case name.
+
+Theorem min_or :
+ forall n m : nat, min n m = n /\ n <= m \/ min n m = m /\ m < n.
+Admitted.
