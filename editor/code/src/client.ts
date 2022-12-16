bef276d86a93a5ab3dc0dd662d72c0177a3016ea
Ramkumar Ramachandra <r@artagnon.com>
2022-12-16 19:18:48 +0100

    editor/code: format ts with prettier
    Prettier is available as a vscode extension, and the only configuration
    it accepts is tab size, which is set to 4 spaces.

--- editor/code/src/client.ts
+++ editor/code/src/client.ts
@@ -1,51 +1,64 @@
-import { window, commands, ExtensionContext, workspace, ViewColumn, Uri } from "vscode";
-import { LanguageClient, LanguageClientOptions, RevealOutputChannelOn } from "vscode-languageclient/node";
+import {
+    window,
+    commands,
+    ExtensionContext,
+    workspace,
+    ViewColumn,
+    Uri,
+} from "vscode";
+import {
+    LanguageClient,
+    LanguageClientOptions,
+    RevealOutputChannelOn,
+} from "vscode-languageclient/node";
 import { GoalPanel } from "./goals";
 
-let client : LanguageClient;
-let goalPanel : GoalPanel | null;
+let client: LanguageClient;
+let goalPanel: GoalPanel | null;
 
-export function panelFactory(context : ExtensionContext) {
-    let panel = window.createWebviewPanel('goals', 'Goals', ViewColumn.Two, {});
-    panel.onDidDispose(() => { goalPanel = null; });
-    const styleUri = panel.webview.asWebviewUri(Uri.joinPath(context.extensionUri, 'media', 'styles.css'));
+export function panelFactory(context: ExtensionContext) {
+    let panel = window.createWebviewPanel("goals", "Goals", ViewColumn.Two, {});
+    panel.onDidDispose(() => {
+        goalPanel = null;
+    });
+    const styleUri = panel.webview.asWebviewUri(
+        Uri.joinPath(context.extensionUri, "media", "styles.css")
+    );
     return new GoalPanel(client, panel, styleUri);
 }
 
-export function activate (context : ExtensionContext) : void {
-    window.showInformationMessage('Going to activate!');
+export function activate(context: ExtensionContext): void {
+    window.showInformationMessage("Going to activate!");
 
-    function coqCommand(command : string, fn : () => void) {
-        let disposable = commands.registerCommand('coq-lsp.'+command, fn);
+    function coqCommand(command: string, fn: () => void) {
+        let disposable = commands.registerCommand("coq-lsp." + command, fn);
         context.subscriptions.push(disposable);
     }
     const restart = () => {
         if (client) {
             client.stop();
-            if(goalPanel) goalPanel.dispose();
+            if (goalPanel) goalPanel.dispose();
         }
 
-        window.showInformationMessage('Going to start!');
+        window.showInformationMessage("Going to start!");
 
-        const config = workspace.getConfiguration('coq-lsp');
+        const config = workspace.getConfiguration("coq-lsp");
         const initializationOptions = {
             eager_diagnostics: config.eager_diagnostics,
-            ok_diagnostics: config.ok_diagnostics
+            ok_diagnostics: config.ok_diagnostics,
         };
 
-        const clientOptions : LanguageClientOptions = {
-            documentSelector: [
-                {scheme: 'file', language: 'coq'}
-            ],
+        const clientOptions: LanguageClientOptions = {
+            documentSelector: [{ scheme: "file", language: "coq" }],
             outputChannelName: "Coq LSP Server Events",
             revealOutputChannelOn: RevealOutputChannelOn.Info,
-            initializationOptions
+            initializationOptions,
         };
         const serverOptions = { command: config.path, args: config.args };
 
         client = new LanguageClient(
-            'coq-lsp-server',
-            'Coq Language Server',
+            "coq-lsp-server",
+            "Coq Language Server",
             serverOptions,
             clientOptions
         );
@@ -53,31 +66,31 @@ export function activate (context : ExtensionContext) : void {
 
         // XXX: Fix this mess with the lifetime of the panel
         goalPanel = panelFactory(context);
-};
+    };
 
     const checkPanelAlive = () => {
-        if(!goalPanel) {
+        if (!goalPanel) {
             goalPanel = panelFactory(context);
         }
-    }
+    };
     const goals = () => {
         checkPanelAlive();
         let uri = window.activeTextEditor?.document?.uri;
         let position = window.activeTextEditor?.selection?.active;
-        if(goalPanel && uri && position) {
+        if (goalPanel && uri && position) {
             goalPanel.update(uri, position);
         }
-    }
+    };
 
-    coqCommand('restart', restart);
-    coqCommand('goals', goals);
+    coqCommand("restart", restart);
+    coqCommand("goals", goals);
 
     restart();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+    if (!client) {
+        return undefined;
+    }
+    return client.stop();
 }
