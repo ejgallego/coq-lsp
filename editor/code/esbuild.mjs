982f8c664a7295141409582d8f4ad379cad792c2
Emilio Jesus Gallego Arias <e+git@x80.org>
2023-07-07 17:41:23 +0200

    [client] [deps] Update to esbuild 0.18
    This has significant API improvements, but it is not a trivial upgrade.

--- editor/code/esbuild.mjs
+++ editor/code/esbuild.mjs
@@ -1,31 +1,40 @@
 #!/usr/bin/env node
+//@ts-check
+
 import process from "process";
 import * as esbuild from "esbuild";
+import { createTextChangeRange } from "typescript";
 
-let watchConfig = (entry) => {
-  return {
-    onRebuild(error, result) {
+// VSCode matcher plugin
+const plugins = [{
+  name: 'vscode-match',
+  setup(build) {
+    let entry = build.initialOptions.entryPoints[0];
+    build.onStart(() => {
       console.log(`[watch] build started (rebuild for ${entry})`);
-      if (error) {
-        error.errors.forEach((error) =>
-          console.error(
-            `> ${error.location.file}:${error.location.line}:${error.location.column}: error: ${error.text}`
-          )
-        );
-      } else console.log(`[watch] build finished (rebuild for ${entry}`);
-    },
-  };
-};
+    });
 
-let watch = process.argv.includes("--watch") ? watchConfig : (entry) => false;
+    build.onEnd(result => {
+      if (result.errors) {
+        result.errors.forEach((error) =>
+          console.error(
+            `> ${error.location.file}:${error.location.line}:${error.location.column}: error: ${error.text}`,
+          ),
+        )
+      } else {
+          console.log(`[watch] build finished (rebuild for ${entry}`);
+      }
+    });
+  }}];
+
+// command line processing
+let watch = process.argv.includes("--watch");
 let minify = process.argv.includes("--minify");
 let disable_sourcemap = process.argv.includes("--sourcemap=no");
 let sourcemap_client = disable_sourcemap ? null : { sourcemap: true };
 let sourcemap_view = disable_sourcemap ? null : { sourcemap: "inline" };
 
-// Build of the VS Code extension, for electron (hence cjs + node)
-var node = esbuild
-  .build({
+const nodeOptions = {
     entryPoints: ["./src/node.ts"],
     bundle: true,
     ...sourcemap_client,
@@ -34,15 +43,9 @@ var node = esbuild
     external: ["vscode"],
     outfile: "out/src/node.js",
     minify,
-    watch: watch("./src/node.ts"),
-  })
-  .then(() => {
-    console.log("[watch] build finished for ./src/node.ts");
-  })
-  .catch(() => process.exit(1));
+};
 
-var browser = esbuild
-  .build({
+var browserOptions = {
     entryPoints: ["./src/browser.ts"],
     bundle: true,
     ...sourcemap_client,
@@ -51,17 +54,11 @@ var browser = esbuild
     external: ["vscode"],
     outfile: "out/src/browser.js",
     minify,
-    watch: watch("./src/browser.ts"),
-  })
-  .then(() => {
-    console.log("[watch] build finished for ./src/browser.ts");
-  })
-  .catch(() => process.exit(1));
+  };
 
-// Build of the VS Code view, for modern Chrome (webview)
-function viewBuild(file) {
-  return esbuild
-    .build({
+  // Build of the VS Code view, for modern Chrome (webview)
+function viewOptions(file) {
+  return {
       entryPoints: [file],
       bundle: true,
       ...sourcemap_view,
@@ -69,15 +66,24 @@ function viewBuild(file) {
       outdir: "out",
       outbase: ".",
       minify,
-      watch: watch(file),
-    })
-    .then(() => {
-      console.log(`[watch] build finished for ${file}`);
-    })
-    .catch(() => process.exit(1));
-}
+    };
+  };
 
-var infoView = viewBuild("./views/info/index.tsx");
-var perfView = viewBuild("./views/perf/index.tsx");
+var infoOptions = viewOptions("./views/info/index.tsx");
+var perfOptions = viewOptions("./views/perf/index.tsx");
 
-await Promise.all[(node, browser, infoView, perfView)];
+const ctxs = [nodeOptions, browserOptions, infoOptions, perfOptions].map((opts) =>
+  //@ts-ignore
+  esbuild.context({...opts, plugins }));
+  
+await Promise.all(ctxs.map((ctx) => {
+    ctx.then((ctx) => {
+      if(watch) {
+        ctx.watch();
+      } else {
+        ctx.rebuild();
+      }
+    })
+  }));
+
+await Promise.all(ctxs.map((ctx) => ctx.then((ctx) => ctx.dispose())));
