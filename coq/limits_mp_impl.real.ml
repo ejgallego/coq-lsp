1720910b4b89b1a1c6c89d20d06e5e4343787ce0
Emilio Jesus Gallego Arias <e+git@x80.org>
2024-04-08 20:17:01 +0200

    [limits] [experiment] Set all limits
    Values are nonsensical tho.

--- coq/limits_mp_impl.real.ml
+++ coq/limits_mp_impl.real.ml
@@ -1,11 +1,36 @@
 (* We'd like to move this code to Lang, but it is still too specific *)
 module Token = Memprof_limits.Token
 
-let start () = Memprof_limits.start_memprof_limits ()
+(* overclock-mode *)
+let oc_mode = false
+
+let oc_init () =
+  let sampling_rate = 1e-2 in
+  let callstack_size = 0 in
+  let tracker = Memprof_limits.Memprof.null_tracker in
+  Memprof_limits.Memprof.start ~sampling_rate ~callstack_size tracker
+
+let start () =
+  if oc_mode then oc_init ();
+  Memprof_limits.start_memprof_limits ();
+  Memprof_limits.set_global_memory_limit 8000000 (* 8 GiB *)
+
+exception Allocations_exceeded of Int64.t
+
+let allocation_limit = Memprof_limits.max_allocation_limit
+(* let allocation_limit = Int64.of_int 1000000000000 *)
 
 let limit ~token ~f x =
   let f () = f x in
-  Memprof_limits.limit_with_token ~token f
+  let limit = allocation_limit in
+  let f () = Memprof_limits.limit_allocations ~limit f in
+  let f () = Memprof_limits.limit_global_memory f in
+  let f () = Memprof_limits.limit_with_token ~token f in
+  match f () with
+  | Ok (Ok (Ok (res, _amount))) -> Ok res
+  | Ok (Ok (Error err)) -> Error err
+  | Ok (Error err) -> Error err
+  | Error err -> Error err
 
 let name () = "memprof-limits"
 let available = true
