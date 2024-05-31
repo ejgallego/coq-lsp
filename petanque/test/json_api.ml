7c1a3e296d9be9a583a3dba1cd362791016c34aa
Emilio Jesus Gallego Arias <e+git@x80.org>
2024-05-31 13:11:22 +0200

    [tests] Another nit to fix from merges.

--- petanque/test/json_api.ml
+++ petanque/test/json_api.ml
@@ -22,8 +22,6 @@ let pp_offset fmt (bp, ep) = Format.fprintf fmt "(%d,%d)" bp ep
 let pp_res_str =
   Coq.Compat.Result.pp Format.pp_print_string Format.pp_print_string
 
-let print_premises = false
-
 let pp_premise fmt
     { Petanque.Agent.Premise.full_name
     ; kind
