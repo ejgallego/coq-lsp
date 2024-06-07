9d79237fa221998e10d4719eab79080b53fe3dd2
Emilio Jesus Gallego Arias <e+git@x80.org>
2024-06-08 01:14:27 +0200

    [petanque] Make http-headers optional for `pet`

--- petanque/json_shell/pet.ml
+++ petanque/json_shell/pet.ml
@@ -1,16 +1,40 @@
 (* json rpc server *)
 open Petanque_json
 
+let use_http_headers = ref true
+
+let read_json inc =
+  match Yojson.Safe.from_channel inc with
+  | json -> Ok json
+  | exception Yojson.Json_error err -> Error err
+
+let read_message inc =
+  if !use_http_headers then Lsp.Io.read_message inc
+  else
+    try
+      match read_json inc with
+      | Error err -> Some (Error err)
+      | Ok json -> Some (Lsp.Base.Message.of_yojson json)
+    with End_of_file -> None
+
+let send_message msg =
+  if !use_http_headers then (
+    Lsp.Io.send_message Format.std_formatter msg;
+    Format.pp_print_flush Format.std_formatter ())
+  else
+    let msg = Lsp.Base.Message.to_yojson msg in
+    Format.fprintf Format.std_formatter "@[%s@]@\n%!"
+      (Yojson.Safe.to_string ?std:None msg)
+(* Format.fprintf Format.std_formatter "@[%a@]@\n%!" Yojson.Safe.pretty_print
+   msg *)
+
 let interp ~token request =
   match Interp.interp ~token request with
   | None -> ()
-  | Some response ->
-    let message = Lsp.Base.Message.response response in
-    Lsp.Io.send_message Format.std_formatter message;
-    Format.pp_print_flush Format.std_formatter ()
+  | Some response -> Lsp.Base.Message.response response |> send_message
 
 let rec loop ~token : unit =
-  match Lsp.Io.read_message stdin with
+  match read_message stdin with
   | None -> () (* EOF *)
   | Some (Ok request) ->
     interp ~token request;
@@ -28,7 +52,7 @@ let trace_notification hdr ?extra msg =
   let notification =
     Lsp.Base.(Notification.(make ~method_ ~params () |> Message.notification))
   in
-  Lsp.Io.send_message Format.std_formatter notification
+  send_message notification
 
 let message_notification ~lvl ~message =
   let module M = Protocol.Message in
@@ -39,11 +63,11 @@ let message_notification ~lvl ~message =
   let notification =
     Lsp.Base.(Notification.(make ~method_ ~params () |> Message.notification))
   in
-  Lsp.Io.send_message Format.std_formatter notification
+  send_message notification
 
 let trace_enabled = true
 
-let pet_main debug roots =
+let pet_main debug roots http_headers =
   Coq.Limits.start ();
   (* Don't trace for now *)
   if trace_enabled then (
@@ -51,9 +75,20 @@ let pet_main debug roots =
     Petanque.Agent.message_ref := message_notification);
   let token = Coq.Limits.Token.create () in
   let () = Utils.set_roots ~token ~debug ~roots in
+  use_http_headers := http_headers;
   loop ~token
 
 open Cmdliner
+
+let http_headers : bool Term.t =
+  let docv = "{yes|no}" in
+  let opts = [ ("yes", true); ("no", false) ] in
+  let absent = "yes" in
+  let doc =
+    "whether http-headers CONTENT-LENGHT are used in the JSON-RPC encoding"
+  in
+  Arg.(
+    value & opt (enum opts) true & info [ "http_headers" ] ~docv ~doc ~absent)
 
 let pet_cmd : unit Cmd.t =
   let doc = "Petanque Coq Environment" in
@@ -66,7 +101,7 @@ let pet_cmd : unit Cmd.t =
   in
   let version = Fleche.Version.server in
   let pet_term =
-    Term.(const pet_main $ Coq.Args.debug $ Coq.Args.roots)
+    Term.(const pet_main $ Coq.Args.debug $ Coq.Args.roots $ http_headers)
     (* const pet_main $ roots $ display $ debug $ plugins $ file $ coqlib *)
     (* $ coqcorelib $ ocamlpath $ rload_path $ load_path $ rifrom) *)
   in
