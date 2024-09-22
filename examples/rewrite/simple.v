0d9954ab06dffd3c678e421d0e78d3a534cee6e4
Emilio Jesus Gallego Arias <e+git@x80.org>
2024-09-22 16:12:52 +0200

    [examples] [rewrite] Add example from Ali, tweak the simple one.

--- examples/rewrite/simple.v
+++ examples/rewrite/simple.v
@@ -5,7 +5,7 @@ Symbols
   (pmul : nat -> nat -> nat).
 
 Notation "a ++ b" := (pplus a b).
-Notation "a ** b" := (pmul a b) (at level 30).
+Notation "a ** b" := (pmul a b) (at level 50).
 
 Rewrite Rules pplus_rewrite :=
 | ?n ++ 0 => ?n
@@ -21,6 +21,5 @@ Rewrite Rules pmul_rewrite :=
 | ?n ** S ?m => ?m ++ (?n ** ?m)
 | ?n ** (?m ** ?o) => (?n ** ?m) ** ?o.
 
-Lemma foo n m : S n ** m ++ 0 = n ++ (n ** m).
+Lemma foo n m : S n ** m ++ 0 = n ++ n ** m.
 Proof. now reflexivity. Qed.
-
